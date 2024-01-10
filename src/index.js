ee443f31c10975c2a634864f2fd4a4dfb79bebfd
aliflikescoding <alif.likes.coding@gmail.com>
2024-01-10 22:36:21 +0800

    Add sticky header feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './scss/main.scss';
+
+window.addEventListener('scroll', () => {
+  let y = window.scrollY;
+
+  let header = document.querySelector(".header");
+  if (y > 240) {
+    header.classList.add("header-ver2");
+  }
+  else {
+    header.classList.add("header-ver1");
+    if(header.classList.contains("header-ver2")) {
+      header.classList.remove("header-ver2");
+    }
+  }
+});
